cecfb5d5934707b2d892199e6752b51b501ce2b0
Braden Purcell <purcellery@ksu.edu>
2019-09-15 22:47:28 -0500

    I made a working program

---
+++ Menu/Entrees/DinoNuggets.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace DinoDiner.Menu.Entrees
+{
+    public class DinoNuggets
+    {
+        private int nuggetCount = 6;
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+                for (int i = 0; i < nuggetCount; i++)
+                {
+                    ingredients.Add("Chicken Nugget");
+                }
+                return ingredients;
+            }
+        }
+        public DinoNuggets()
+        {
+            this.Price = 4.25;
+            this.Calories = 354;
+        }    
+        public void AddNugget()
+        {
+            nuggetCount++;
+            this.Price += .25;
+            this.Calories += 59;
+        }
+    }
+}
